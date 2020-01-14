cfe3fe9289890e46ec8838fab01d839f8db48b3b
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2020-01-13 17:03:03 -0700

    adding first wave of stuff

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,20 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(x):
+    if x % 2 == 0:
+        print(x, "is an Even number")
+    else:
+        print(x, "is an Odd Number")
+    return 
+
 
 # Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
-
+#num = input("Enter a number: ")
+#num = int(num)
+''' Commented out the input statements because they 
+ dont run on VS code, but do work in terminal'''   
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+is_even(420)
